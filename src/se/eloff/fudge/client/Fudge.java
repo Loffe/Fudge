9228db78066184f57a854b13561723b9eea41445
Thor Bossuyt <thobo260@zaza12.ida.liu.se>
2011-02-22 14:36:57 +0100

    git test

--- src/se/eloff/fudge/client/Fudge.java
+++ src/se/eloff/fudge/client/Fudge.java
@@ -41,7 +41,7 @@ public class Fudge implements EntryPoint {
 	public void onModuleLoad() {
 		final Button sendButton = new Button("Send");
 		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
+		nameField.setText("Eloff");
 		final Label errorLabel = new Label();
 
 		// We can add style names to widgets
