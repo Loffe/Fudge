891fcf3568b0ed924b3a611d4383e4b6bbe35752
Erik Eloff <eriel743@student.liu.se>
2011-02-22 13:53:11 +0100

    Initial commit

---
+++ src/se/eloff/fudge/server/GreetingServiceImpl.java
@@ -0,0 +1,28 @@
+package se.eloff.fudge.server;
+
+import se.eloff.fudge.client.GreetingService;
+import se.eloff.fudge.shared.FieldVerifier;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * The server side implementation of the RPC service.
+ */
+@SuppressWarnings("serial")
+public class GreetingServiceImpl extends RemoteServiceServlet implements
+		GreetingService {
+
+	public String greetServer(String input) throws IllegalArgumentException {
+		// Verify that the input is valid. 
+		if (!FieldVerifier.isValidName(input)) {
+			// If the input is not valid, throw an IllegalArgumentException back to
+			// the client.
+			throw new IllegalArgumentException(
+					"Name must be at least 4 characters long");
+		}
+
+		String serverInfo = getServletContext().getServerInfo();
+		String userAgent = getThreadLocalRequest().getHeader("User-Agent");
+		return "Hello, " + input + "!<br><br>I am running " + serverInfo
+				+ ".<br><br>It looks like you are using:<br>" + userAgent;
+	}
+}
