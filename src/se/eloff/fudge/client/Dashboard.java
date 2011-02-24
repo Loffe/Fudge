a96d40d50625057748dd4eca52446c87bb04adbd
Thor Bossuyt <thobo260@student.liu.se>
2011-02-24 15:03:18 +0100

    skapat dashboard

---
+++ src/se/eloff/fudge/client/Dashboard.java
@@ -0,0 +1,14 @@
+package se.eloff.fudge.client;
+
+import se.eloff.fudge.client.bean.User;
+
+import com.smartgwt.client.widgets.Canvas;
+import com.smartgwt.client.widgets.Label;
+
+public class Dashboard extends Canvas {
+	public Dashboard(User user) {
+		Label label = new Label("Hello" + user.getUserName());
+		
+		this.addChild(label);
+	}
+}
---
+++ src/se/eloff/fudge/client/bean/User.java
@@ -0,0 +1,28 @@
+package se.eloff.fudge.client.bean;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+  /**
+  * Add this variable for serialization
+  */
+  private static final long serialVersionUID = 1L;
+ 
+  private String userName;
+  private String password;
+ 
+  public String getUserName() {
+    return userName;
+  }
+ 
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+  public String getPassword() {
+ 	return password;
+  }
+  public void setPassword(String password) {
+	this.password = password;
+ }
+ 
+}
