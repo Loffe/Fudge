7a1b5a0436d7ae6d65f8de704cc6a7ca22f77240
Erik Eloff <eriel743@student.liu.se>
2011-03-03 11:47:19 +0100

    Added PostServiceImpl

---
+++ src/se/eloff/fudge/server/PostServiceImpl.java
@@ -0,0 +1,45 @@
+package se.eloff.fudge.server;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import se.eloff.fudge.client.PostService;
+import se.eloff.fudge.client.bean.Post;
+import se.eloff.fudge.client.bean.Topic;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+public class PostServiceImpl extends RemoteServiceServlet implements PostService {
+
+	@Override
+	public boolean createPost(Post post) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deletePost(Post post) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean editPost(Post post) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Post[] getAllPosts(Topic topic) {
+		DatabaseManager database = DatabaseManager.getInstance();
+		Connection conn = database.getConnection();
+
+		try {
+			return database.getAllPosts(conn, topic);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+}
