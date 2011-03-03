bb0525c094b878d230e23342a372592e0f7cf5f9
Erik Eloff <eriel743@student.liu.se>
2011-03-03 11:01:02 +0100

    Implemented Post class

--- src/se/eloff/fudge/client/bean/Post.java
+++ src/se/eloff/fudge/client/bean/Post.java
@@ -1,5 +1,65 @@
 package se.eloff.fudge.client.bean;
 
-public class Post {
+import java.io.Serializable;
+import java.sql.Date;
 
+public class Post implements Serializable {
+	private static final long serialVersionUID = 3211305636644559351L;
+	
+	private int id;
+	private int topicId;
+	private int userId;
+	private Date postedOnDate;
+	private String message;
+	
+	public Post() {
+	}
+	
+	public Post(int id, int topicId, int userId, Date postedOnDate,
+			String message) {
+		this.id = id;
+		this.topicId = topicId;
+		this.userId = userId;
+		this.postedOnDate = postedOnDate;
+		this.message = message;
+	}
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getTopicId() {
+		return topicId;
+	}
+
+	public void setTopicId(int topicId) {
+		this.topicId = topicId;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public Date getPostedOnDate() {
+		return postedOnDate;
+	}
+
+	public void setPostedOnDate(Date postedOnDate) {
+		this.postedOnDate = postedOnDate;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
 }
