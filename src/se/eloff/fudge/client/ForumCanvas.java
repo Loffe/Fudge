d038dba681d7efbe4038ad0f04ad38513b6cad08
Erik Eloff <eriel743@student.liu.se>
2011-03-10 17:08:21 +0100

    Freshen up forum view

--- src/se/eloff/fudge/client/ForumCanvas.java
+++ src/se/eloff/fudge/client/ForumCanvas.java
@@ -6,16 +6,18 @@ import se.eloff.fudge.client.bean.Topic;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.smartgwt.client.types.Cursor;
 import com.smartgwt.client.widgets.Button;
 import com.smartgwt.client.widgets.Canvas;
-import com.smartgwt.client.widgets.Label;
+import com.smartgwt.client.widgets.HTMLFlow;
 import com.smartgwt.client.widgets.Window;
 import com.smartgwt.client.widgets.events.ClickEvent;
 import com.smartgwt.client.widgets.events.ClickHandler;
-import com.smartgwt.client.widgets.layout.HStack;
-import com.smartgwt.client.widgets.layout.VStack;
+import com.smartgwt.client.widgets.layout.HLayout;
+import com.smartgwt.client.widgets.layout.Layout;
+import com.smartgwt.client.widgets.layout.VLayout;
 
 public class ForumCanvas extends ItemCanvas<Forum, Topic> {
 
@@ -49,31 +51,57 @@ public class ForumCanvas extends ItemCanvas<Forum, Topic> {
 
 	@Override
 	protected Canvas createItem(final Topic topic) {
-		HStack hstack = new HStack();
+		Post post = topic.getPost();
+		if (post == null)
+			throw new NullPointerException(
+					"Topic without post cannot be added to canvas");
+
+		Layout hstack = new HLayout();
+		hstack.setPadding(5);
 		hstack.setShowEdges(true);
+		hstack.setWidth100();
+		hstack.setAutoHeight();
 		hstack.setStyleName("topic");
+		hstack.setCursor(Cursor.HAND);
 
-		VStack vstack = new VStack();
-		Label topiclabel = new Label(topic.getName());
+		// Left column
+		Layout vstack = new VLayout();
+
+		Canvas topiclabel = new HTMLFlow(topic.getName());
 		topiclabel.setStyleName("topicName");
-		topiclabel.setWidth(400);
-		topiclabel.setHeight(40);
-		topiclabel.setCursor(Cursor.HAND);
 
-		// TODO: add click handlers and number of replies
+		Canvas message = new HTMLFlow(post.getMessage());
+		message.setStyleName("message");
 
-		topiclabel.addClickHandler(new ClickHandler() {
+		// Right column
+		Layout rightCol = new VLayout();
+		rightCol.setWidth(200);
+
+		DateTimeFormat formatter = DateTimeFormat
+				.getFormat("yyyy-MM-dd HH:mm:ss");
+		Canvas dateLabel = new HTMLFlow(formatter
+				.format(post.getPostedOnDate()));
+		dateLabel.setStyleName("date");
+
+		Canvas nrOfReplies = new HTMLFlow("test");
+
+		vstack.addMember(topiclabel);
+		vstack.addMember(message);
+
+		rightCol.addMember(dateLabel);
+		rightCol.addMember(nrOfReplies);
+
+		// TODO: add number of replies
+
+		hstack.addMember(vstack);
+		hstack.addMember(rightCol);
+
+		hstack.addClickHandler(new ClickHandler() {
 
 			public void onClick(ClickEvent event) {
 				bus.fireEvent(new TopicEvent(topic));
 			}
 		});
-
-		vstack.addMember(topiclabel);
-		vstack.addMember(new Label(topic.getPost().getMessage()));
-
-		hstack.addMember(vstack);
-		// hstack.addMember(new Label(String.valueOf(topic.getNrOfTopics())));
 
 		return hstack;
 	}
